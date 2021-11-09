a98356256f7c3b785a814fea859bfca722299eaa
LuanaMB <luanabragaaa@hotmail.com>
2021-11-09 15:59:34 -0300

    requisito 17

--- src/Pages/Receitas.js
+++ src/Pages/Receitas.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ButtonSearch from '../Components/ButtonSearch';
 import Header from '../Components/Header';
+import ReceitasContext from '../Context/ReceitasContext';
 
 function Receitas() {
+  const { dataApi } = useContext(ReceitasContext);
+  const NUMBER = 12;
   return (
-    <Header title="Comidas">
-      <ButtonSearch />
-    </Header>
+    <div>
+      <Header title="Comidas">
+        <ButtonSearch />
+      </Header>
+      {dataApi.meals && dataApi.meals.slice(0, NUMBER)
+        .map(({ idMeal, strMeal, strMealThumb }, index) => (
+          <div key={ idMeal } data-testid={ `${index}-recipe-card` }>
+            <img
+              className="img-meals"
+              src={ strMealThumb }
+              alt={ strMeal }
+              data-testid={ `${index}-card-img` }
+            />
+            <h3 data-testid={ `${index}-card-name` }>{strMeal}</h3>
+          </div>))}
+    </div>
   );
 }
 
