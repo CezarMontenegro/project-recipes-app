35577df4c713fed0fad2c0bde9a576945188ef07
Julio <julioaranabr@yahoo.com.br>
2021-11-16 15:02:22 -0300

    started req-43

--- src/Pages/ComidasDetalhes.js
+++ src/Pages/ComidasDetalhes.js
@@ -1,18 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router';
 import ReceitasContext from '../Context/ReceitasContext';
 import shareIcon from '../images/shareIcon.svg';
-import blackHeartIcon from '../images/blackHeartIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import { urlIdFood, urlNameBebidas } from '../helper/helper';
 import FoodIngredients from '../Components/FoodIngredients';
+
+const copy = require('clipboard-copy');
 
 function ComidaDetalhes() {
   const { dataIdCard, getCardById, getAPIname } = useContext(ReceitasContext);
   const { id } = useParams();
+  const history = useHistory();
+  const { location: { pathname } } = history;
 
   useEffect(() => { getCardById(urlIdFood, id); }, []);
   useEffect(() => { getAPIname(urlNameBebidas, ''); }, []);
+
+  const handleShare = () => {
+    copy(pathname);
+    global.alert('Link Copiado!');
+  };
 
   return (
     <section>
@@ -30,16 +40,18 @@ function ComidaDetalhes() {
             <div className="card-body card-detalhes">
               <h5 className="card-text" data-testid="recipe-title">{strMeal}</h5>
               <div>
-                <img
+                <input
+                  type="image"
                   className="d-inline-block align-top"
                   data-testid="share-btn"
                   src={ shareIcon }
                   alt="Ícone de compartilhamento"
+                  onClick={ handleShare }
                 />
                 <img
                   className="d-inline-block align-top"
                   data-testid="favorite-btn"
-                  src={ blackHeartIcon }
+                  src={ whiteHeartIcon }
                   alt="Ícone de favoritar"
                 />
               </div>
