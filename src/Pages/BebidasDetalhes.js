f74a8dfda957ecd59174f37cd738045bdf2ab064
Cezar <cezarguimaraes@gmail.com>
2021-11-16 17:48:09 -0300

    Requisitos 44 ao 46 concluidos

--- src/Pages/BebidasDetalhes.js
+++ src/Pages/BebidasDetalhes.js
@@ -1,17 +1,71 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router';
 import ReceitasContext from '../Context/ReceitasContext';
 import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import { urlIdDrink, urlNameComidas } from '../helper/helper';
 import DrinksIngredients from '../Components/DrinksIngredients';
 
+const copy = require('clipboard-copy');
+
 function BebidasDetalhes() {
   const { dataIdCard, getCardById, getAPIname } = useContext(ReceitasContext);
   const { id } = useParams();
+  const history = useHistory();
+  const { location: { pathname } } = history;
+  const [isFavorite, setIsfavorite] = useState(false);
+
   useEffect(() => { getCardById(urlIdDrink, id); }, []);
   useEffect(() => { getAPIname(urlNameComidas, ''); }, []);
+  useEffect(() => {
+    if (!localStorage.favoriteRecipes) {
+      setIsfavorite(false);
+    } else {
+      const getStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      if (dataIdCard.drinks) {
+        const verifyIfExists = getStorage.some((obj) => obj.id
+      === dataIdCard.drinks[0].idDrink);
+        setIsfavorite(verifyIfExists);
+      }
+    }
+  }, [dataIdCard]);
+
+  const handleShare = () => {
+    copy(pathname);
+    global.alert('Link Copiado!');
+  };
+
+  const handleFavorite = () => {
+    setIsfavorite(!isFavorite);
+    if (!localStorage.favoriteRecipes) {
+      localStorage.setItem('favoriteRecipes',
+        JSON.stringify([{
+          id: dataIdCard.drinks && dataIdCard.drinks[0].idDrink,
+          type: 'bebida',
+          area: '',
+          category: dataIdCard.drinks && dataIdCard.drinks[0].strCategory,
+          alcoholicOrNot: dataIdCard.drinks && dataIdCard.drinks[0].strAlcoholic,
+          name: dataIdCard.drinks && dataIdCard.drinks[0].strDrink,
+          image: dataIdCard.drinks && dataIdCard.drinks[0].strDrinkThumb,
+        }]));
+    } else {
+      const getStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      getStorage.concat({
+        id: dataIdCard.drinks && dataIdCard.drinks[0].idDrink,
+        type: 'bebida',
+        area: '',
+        category: dataIdCard.drinks && dataIdCard.drinks[0].strCategory,
+        alcoholicOrNot: dataIdCard.drinks && dataIdCard.drinks[0].strAlcoholic,
+        name: dataIdCard.drinks && dataIdCard.drinks[0].strDrink,
+        image: dataIdCard.drinks && dataIdCard.drinks[0].strDrinkThumb,
+      });
+      localStorage.setItem('favoriteRecipes',
+        JSON.stringify(getStorage));
+    }
+  };
 
   return (
     <section>
@@ -29,17 +83,21 @@ function BebidasDetalhes() {
             <div className="card-body card-detalhes">
               <h5 className="card-text" data-testid="recipe-title">{strDrink}</h5>
               <div>
-                <img
+                <input
+                  type="image"
                   className="d-inline-block align-top"
                   data-testid="share-btn"
                   src={ shareIcon }
                   alt="Ícone de compartilhamento"
+                  onClick={ handleShare }
                 />
-                <img
+                <input
+                  type="image"
                   className="d-inline-block align-top"
                   data-testid="favorite-btn"
-                  src={ blackHeartIcon }
+                  src={ isFavorite ? blackHeartIcon : whiteHeartIcon }
                   alt="Ícone de favoritar"
+                  onClick={ handleFavorite }
                 />
               </div>
             </div>
--- src/Pages/ComidasDetalhes.js
+++ src/Pages/ComidasDetalhes.js
@@ -1,10 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useHistory } from 'react-router';
 import ReceitasContext from '../Context/ReceitasContext';
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 import { urlIdFood, urlNameBebidas } from '../helper/helper';
 import FoodIngredients from '../Components/FoodIngredients';
 
@@ -15,13 +16,55 @@ function ComidaDetalhes() {
   const { id } = useParams();
   const history = useHistory();
   const { location: { pathname } } = history;
+  const [isFavorite, setIsfavorite] = useState(false);
 
   useEffect(() => { getCardById(urlIdFood, id); }, []);
   useEffect(() => { getAPIname(urlNameBebidas, ''); }, []);
+  useEffect(() => {
+    if (!localStorage.favoriteRecipes) {
+      setIsfavorite(false);
+    } else {
+      const getStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      if (dataIdCard.meals) {
+        const verifyIfExists = getStorage.some((obj) => obj.id
+      === dataIdCard.meals[0].idMeal);
+        setIsfavorite(verifyIfExists);
+      }
+    }
+  }, [dataIdCard]);
 
   const handleShare = () => {
     copy(pathname);
     global.alert('Link Copiado!');
+  };
+
+  const handleFavorite = () => {
+    setIsfavorite(!isFavorite);
+    if (!localStorage.favoriteRecipes) {
+      localStorage.setItem('favoriteRecipes',
+        JSON.stringify([{
+          id: dataIdCard.meals && dataIdCard.meals[0].idMeal,
+          type: 'comida',
+          area: dataIdCard.meals && dataIdCard.meals[0].strArea,
+          category: dataIdCard.meals && dataIdCard.meals[0].strCategory,
+          alcoholicOrNot: '',
+          name: dataIdCard.meals && dataIdCard.meals[0].strMeal,
+          image: dataIdCard.meals && dataIdCard.meals[0].strMealThumb,
+        }]));
+    } else {
+      const getStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      getStorage.concat({
+        id: dataIdCard.meals && dataIdCard.meals[0].idMeal,
+        type: 'comida',
+        area: dataIdCard.meals && dataIdCard.meals[0].strArea,
+        category: dataIdCard.meals && dataIdCard.meals[0].strCategory,
+        alcoholicOrNot: '',
+        name: dataIdCard.meals && dataIdCard.meals[0].strMeal,
+        image: dataIdCard.meals && dataIdCard.meals[0].strMealThumb,
+      });
+      localStorage.setItem('favoriteRecipes',
+        JSON.stringify(getStorage));
+    }
   };
 
   return (
@@ -48,11 +91,13 @@ function ComidaDetalhes() {
                   alt="Ícone de compartilhamento"
                   onClick={ handleShare }
                 />
-                <img
+                <input
+                  type="image"
                   className="d-inline-block align-top"
                   data-testid="favorite-btn"
-                  src={ whiteHeartIcon }
+                  src={ isFavorite ? blackHeartIcon : whiteHeartIcon }
                   alt="Ícone de favoritar"
+                  onClick={ handleFavorite }
                 />
               </div>
             </div>
