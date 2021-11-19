0660366fd859cd9e31a82625bfc51e0b90fadd50
LuanaMB <luanabragaaa@hotmail.com>
2021-11-19 17:14:55 -0300

    requisitos 60, 61, 62 e parte do 64

---
+++ src/Components/CardDrinkFavorite.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+
+function CardDrinkFavorite({ name, image, alcoholicOrNot, index, id }) {
+  const [isFavorited, setIsFavorited] = useState();
+
+  // https://pt.stackoverflow.com/questions/229222/como-excluir-um-produto-com-id-no-sessionstorage
+  function removeItem(idCard) {
+    const obj = JSON.parse(localStorage.getItem('favoriteRecipes')) || {}; // localStorage Nome
+    for (let i = 0; i < obj.length; i += 1) { // loop para buscar o id
+      if (obj[i].id === idCard) { // verifica id
+        obj.splice(i, 1); // remove item
+        break; // finaliza o loop
+      }
+    }
+    localStorage.setItem('favoriteRecipes', JSON.stringify(obj)); // reescreve a localStorage
+  }
+
+  const setFavorited = (idFood) => {
+    if (isFavorited) {
+      setIsFavorited(false);
+    } else {
+      removeItem(idFood);
+      setIsFavorited(true);
+    }
+  };
+  return (
+    <section className="display-card">
+      <img
+        src={ image }
+        alt={ `imagem do prato ${name}` }
+        data-testid={ `${index}-horizontal-image` }
+      />
+      <h3 data-testid={ `${index}-horizontal-name` }>
+        {name}
+      </h3>
+      <p data-testid={ `${index}-horizontal-top-text` }>
+        Tipo de bebida:
+        {alcoholicOrNot}
+      </p>
+      <button type="button">
+        <img
+          src={ shareIcon }
+          alt="Imagem de compartilhar"
+          data-testid={ `${index}-horizontal-share-btn` }
+        />
+      </button>
+      <button type="button" onClick={ () => setFavorited(id) }>
+        <img
+          src={ isFavorited ? whiteHeartIcon : blackHeartIcon }
+          alt="Imagem de coração para favoritar e desfavoritar"
+          data-testid={ `${index}-horizontal-favorite-btn` }
+        />
+      </button>
+    </section>
+  );
+}
+
+CardDrinkFavorite.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  alcoholicOrNot: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+export default CardDrinkFavorite;
--- src/Pages/ReceitasFavoritas.js
+++ src/Pages/ReceitasFavoritas.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import CardDrinkFavorite from '../Components/CardDrinkFavorite';
+import CardFoodFavorite from '../Components/CardFoodFavorite';
 import Header from '../Components/Header';
 
 function ReceitasFavoritas() {
+  const getFavoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
   return (
     <section>
       <Header title="Receitas Favoritas" />
+      <section>
+        <button type="button" data-testid="filter-by-food-btn">Food</button>
+        <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+        <button type="button" data-testid="filter-by-all-btn">All</button>
+        {getFavoriteRecipes
+          .map(({ name, type, category, area, image, id, alcoholicOrNot }, index) => (
+            <section key={ id }>
+              { type === 'comida' ? <CardFoodFavorite
+                name={ name }
+                category={ category }
+                image={ image }
+                area={ area }
+                index={ index }
+                id={ id }
+              />
+                : (
+                  <CardDrinkFavorite
+                    alcoholicOrNot={ alcoholicOrNot }
+                    name={ name }
+                    image={ image }
+                    index={ index }
+                  />)}
+            </section>
+          )) }
+      </section>
     </section>
   );
 }
