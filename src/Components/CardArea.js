fe77b0d4461a81403670aee645b7dc02ef7c5abf
LuanaMB <luanabragaaa@hotmail.com>
2021-11-20 10:11:08 -0300

    troca por link

--- src/Components/CardArea.js
+++ src/Components/CardArea.js
@@ -1,43 +1,44 @@
 import React, { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import ReceitasContext from '../Context/ReceitasContext';
 
 const CardArea = () => {
   const { meals } = useContext(ReceitasContext);
   const NUMBER = 12;
-  const history = useHistory();
-
-  const redirect = (param) => {
-    history.push(`/comidas/${param}`);
-  };
 
   return (
     <section className="display-card">
       {meals.meals && meals.meals.slice(0, NUMBER)
         .map(({ idMeal, strMeal, strMealThumb }, index) => (
-          <div
+          <Link
             key={ idMeal }
-            data-testid={ `${index}-recipe-card` }
-            className="card div-card col-sm-2 card-food"
+            to={ `/bebidas/${idMeal}` }
           >
-            <input
-              className="card-img-top"
-              type="image"
-              src={ strMealThumb }
-              alt={ strMeal }
-              data-testid={ `${index}-card-img` }
-              onClick={ () => redirect(idMeal) }
-            />
-            <div className="card-body">
-              <h5
-                className="card-text"
-                data-testid={ `${index}-card-name` }
-              >
-                {strMeal}
+            <div
+              key={ idMeal }
+              data-testid={ `${index}-recipe-card` }
+              className="card div-card col-sm-2 card-food"
+            >
+              <input
+                className="card-img-top"
+                type="image"
+                src={ strMealThumb }
+                alt={ strMeal }
+                data-testid={ `${index}-card-img` }
+                onClick={ () => redirect(idMeal) }
+              />
+              <div className="card-body">
+                <h5
+                  className="card-text"
+                  data-testid={ `${index}-card-name` }
+                >
+                  {strMeal}
 
-              </h5>
+                </h5>
+              </div>
             </div>
-          </div>))}
+          </Link>
+        ))}
     </section>
   );
 };
