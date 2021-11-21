c0974e64531145957a4b3d9e5d980817813270d7
Julio <julioaranabr@yahoo.com.br>
2021-11-21 11:58:23 -0300

    refatored cardFood

--- src/Components/CardDrink.js
+++ src/Components/CardDrink.js
@@ -1,42 +1,41 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router';
 import ReceitasContext from '../Context/ReceitasContext';
 
 const CardDrink = () => {
   const { dataApi } = useContext(ReceitasContext);
   const NUMBER = 12;
+  const history = useHistory();
+
+  const handleClick = (param) => history.push(`/comidas/${param}`);
 
   return (
     <section className="display-card">
       {dataApi.drinks && dataApi.drinks.slice(0, NUMBER)
         .map(({ strDrink, idDrink, strDrinkThumb }, index) => (
-          <Link
+          <div
             key={ idDrink }
-            to="/bebidas"
+            data-testid={ `${index}-recipe-card` }
+            className="card div-card col-sm-2 card-food"
           >
-            <div
-              key={ idDrink }
-              data-testid={ `${index}-recipe-card` }
-              className="card div-card col-sm-2 card-food"
-            >
-              <input
-                type="image"
-                className="card-img-top"
-                src={ strDrinkThumb }
-                alt={ strDrink }
-                data-testid={ `${index}-card-img` }
-              />
-              <div className="card-body">
-                <h5
-                  className="card-text"
-                  data-testid={ `${index}-card-name` }
-                >
-                  {strDrink}
+            <input
+              className="card-img-top"
+              type="image"
+              src={ strDrinkThumb }
+              alt={ strDrink }
+              data-testid={ `${index}-card-img` }
+              onClick={ () => handleClick(idDrink) }
+            />
+            <div className="card-body">
+              <h5
+                className="card-text"
+                data-testid={ `${index}-card-name` }
+              >
+                {strDrink}
 
-                </h5>
-              </div>
+              </h5>
             </div>
-          </Link>
+          </div>
         ))}
     </section>
   );
--- src/Components/CardFood.js
+++ src/Components/CardFood.js
@@ -1,42 +1,41 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router';
 import ReceitasContext from '../Context/ReceitasContext';
 
 const CardFood = () => {
   const { dataApi } = useContext(ReceitasContext);
   const NUMBER = 12;
+  const history = useHistory();
+
+  const handleClick = (param) => history.push(`/comidas/${param}`);
 
   return (
     <section className="display-card">
       {dataApi.meals && dataApi.meals.slice(0, NUMBER)
         .map(({ idMeal, strMeal, strMealThumb }, index) => (
-          <Link
+          <div
             key={ idMeal }
-            to="/comidas/"
+            data-testid={ `${index}-recipe-card` }
+            className="card div-card col-sm-2 card-food"
           >
-            <div
-              key={ idMeal }
-              data-testid={ `${index}-recipe-card` }
-              className="card div-card col-sm-2 card-food"
-            >
-              <input
-                className="card-img-top"
-                type="image"
-                src={ strMealThumb }
-                alt={ strMeal }
-                data-testid={ `${index}-card-img` }
-              />
-              <div className="card-body">
-                <h5
-                  className="card-text"
-                  data-testid={ `${index}-card-name` }
-                >
-                  {strMeal}
-                </h5>
-              </div>
+            <input
+              className="card-img-top"
+              type="image"
+              src={ strMealThumb }
+              alt={ strMeal }
+              data-testid={ `${index}-card-img` }
+              onClick={ () => handleClick(idMeal) }
+            />
+            <div className="card-body">
+              <h5
+                className="card-text"
+                data-testid={ `${index}-card-name` }
+              >
+                {strMeal}
+
+              </h5>
             </div>
-          </Link>
-        ))}
+          </div>))}
     </section>
   );
 };
